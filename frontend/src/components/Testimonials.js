1e2becaf0cb45fbf3ae6b64d4323bbf20f7f151b
jeonyounsooesa <youggamhannona@gmail.com>
2024-01-20 04:57:59 +0600

    cleaning the code

--- frontend/src/components/Testimonials.js
+++ frontend/src/components/Testimonials.js
@@ -40,10 +40,6 @@ export default function Testimonials() {
      </div>
         
           <div className="container px-5 py-10 mx-auto text-center">
-            {/* <UsersIcon className="w-10 inline-block mb-4" />
-            <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
-              Crtfct
-            </h1> */}
             <div className="inline-flex items-center">
             <div className="h-full bg-gray-800 bg-opacity-50 p-8 rounded">
               <img alt="Google" src={Google}/>
